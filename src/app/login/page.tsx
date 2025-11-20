c2ea6773cb1b0c0d23ac99c845ef78e3795f759e
atontalapur <advaith.mysuru@gmail.com>
2025-11-20 05:45:54 -0800

    ADDED reg ex for quotes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -277,7 +277,7 @@ export default function Login() {
             </div>
             
             <p className="text-unifolio-mediumgray mb-4">
-              Enter your email address and we'll send you a link to reset your password.
+              Enter your email address and we&apos;ll send you a link to reset your password.
             </p>
             
             <form onSubmit={handleForgotPassword} className="space-y-4">
